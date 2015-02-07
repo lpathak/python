1b3fa4bd0720874e606f9c1ba0e0e623acc1083a
Lokesh Pathak <pathak.lokesh@hotmail.com>
2015-02-07 01:09:27 -0800

    compute year

---
+++ perl/year.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use 5.010;
+
+if ($year_according_to_computer == 1900) {
+    say "Y2K has doomed us all!  Everyone to the compound.";
+}
+
+unless ($bank_account > 0) {
+    say "I'm broke!";
+}
